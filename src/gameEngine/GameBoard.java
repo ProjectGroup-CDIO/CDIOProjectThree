0f6ce508dd1c23ec161c3eaa27a54e4c652b1edc
THOMAS <thomas@liljegren.dk>
2014-11-20 20:55:52 +0100

    landOnField i Refuge ændret lidt, owner fjernet fra constructor i Refuge. GameBoard er halvvejs

--- src/gameEngine/GameBoard.java
+++ src/gameEngine/GameBoard.java
@@ -2,14 +2,24 @@ package gameEngine;
 
 public class GameBoard {
 	
-	Fields[] field = new Fields[21];
-	for(int i = 0; i < field.length(); i++){
-		field[i] = new Territory();
-	}
+	Fields[] fields = new Fields[21]{
+		new Territory("Tribe Encampment", 1, 1000, 100),
+		new Territory("Crater", 2, 1500, 300),
+		new Territory("Mountain", 3, 2000, 500), 
+		new Territory("Cold Desert", 4, 3000, 700),
+		new Territory("Black Cave", 5, 4000, 1000),
+		new Territory("The Werewall", 6, 4300, 1300),
+		new Territory("Mountain Village", 7, 4750, 1600),
+		new Territory("South Citadel", 8, 5000, 2000),
+		new Territory("Palace Gates", 9, 5500, 2600),
+		new Territory("Tower", 10, 6000, 3200),
+		new Territory("Castle", 11, 8000, 4000),
+		new Refuge()
 	
 	field[0] = terr1;
 	terr1 = new Territory("Tribe Encampment", 1, 1000, 100);
 	
+}
 }
 
 
--- src/gameEngine/Refuge.java
+++ src/gameEngine/Refuge.java
@@ -4,17 +4,23 @@ public class Refuge extends Fields{
 
 	private int bonus;
 	
-	public Refuge(String fieldName, int fieldNumber, Player playerWhoLandedOnField, int bonus){
+	public Refuge(String fieldName, int fieldNumber, int bonus){
 		super(fieldName, fieldNumber);
+		this.bonus = bonus;
 	}
 	
-	public void landOnField(Player playerWhoLandedOnField){
-		if (fieldName.equals("Monastery")){
-			bonus= 500; //Monestery gives the player 500
-		}
-		else{
-			bonus = 5000; //The Only other refuge is the Walled City which gives 5000
-		}
-		playerWhoLandedOnField.getAccount().deposit(bonus); //Deposit the bonus to the player
+	@Override
+	public void landOnField(Player playerWhoLandedOnField) {
+		playerWhoLandedOnField.getAccount().deposit(bonus);
 	}
+	
+//	public void landOnField(Player playerWhoLandedOnField){
+//		if (fieldName.equals("Monastery")){
+//			bonus= 500; //Monestery gives the player 500
+//		}
+//		else{
+//			bonus = 5000; //The Only other refuge is the Walled City which gives 5000
+//		}
+//		playerWhoLandedOnField.getAccount().deposit(bonus); //Deposit the bonus to the player
+	
 }
