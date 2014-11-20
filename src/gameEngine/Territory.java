99e21cd6cab52ac2fc704e80ca2bcbce1f9b6c69
naelrashdeen <NaelRashdeen@w00000.web1.wireless.dtu.dk>
2014-11-20 15:13:08 +0100

    Opdateret territory klassen. Tjekket efter med J-unit test

--- src/gameEngine/Territory.java
+++ src/gameEngine/Territory.java
@@ -5,15 +5,32 @@ public class Territory extends Ownable {
 	private int rent; 
 	
 	public Territory(String fieldName, Player owner, int price, int rent) {
-		super(fieldName, owner, price);
+		super(fieldName, rent, owner, price);
 		this.rent = rent;
 	}
-	private String name = "Tribe Encampent";
-		public void completeRent(Player owner, Player lander){
+		
+	/*
+	 * Withdraws the points from the lander and deposits them to the owner
+	 */
+	/**
+	 * 
+	 * @param owner is the player who owns the field
+	 * @param lander is the player who lands on the field
+	 */
+	public void completeRent(Player owner, Player lander){
 		lander.getAccount().withdraw(rent);
 		owner.getAccount().deposit(rent);
-	
 	}
+		@Override
+		public int getRent() {
+			// TODO Auto-generated method stub
+			return rent;
+		}
+		@Override
+		public void landOnField(Player player) {
+			// TODO Auto-generated method stub
+			
+		}
 	
 }
 
