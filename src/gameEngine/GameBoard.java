a7c19966f37d85d96004134cdefe9784a640ccd3
THOMAS <thomas@liljegren.dk>
2014-11-20 20:14:11 +0100

    lidt ting rettet i Fleet og Territory

--- src/gameEngine/GameBoard.java
+++ src/gameEngine/GameBoard.java
@@ -2,7 +2,7 @@ package gameEngine;
 
 public class GameBoard {
 	
-	Fields[] field = new Fields[40];
+	Fields[] field = new Fields[21];
 	
 	field[0] = 
 }
--- src/gameEngine/Territory.java
+++ src/gameEngine/Territory.java
@@ -4,33 +4,25 @@ public class Territory extends Ownable {
 	
 	private int rent; 
 	
-	public Territory(String fieldName, Player owner, int price, int rent) {
-		super(fieldName, rent, owner, price);
+	public Territory(String fieldName, int fieldNumber, Player owner, int price, int rent) {
+		super(fieldName, fieldNumber, owner, price);
 		this.rent = rent;
 	}
 		
-	/*
-	 * Withdraws the points from the lander and deposits them to the owner
-	 */
+
 	/**
-	 * 
-	 * @param owner is the player who owns the field
+	 *Withdraws the points from the lander and deposits them to the owner
 	 * @param lander is the player who lands on the field
 	 */
-	public void completeRent(Player owner, Player lander){
+	public void landOnField( Player lander){
 		lander.getAccount().withdraw(rent);
-		owner.getAccount().deposit(rent);
+		super.getOwner().getAccount().deposit(rent);
 	}
-		@Override
-		public int getRent() {
-			// TODO Auto-generated method stub
-			return rent;
-		}
-		@Override
-		public void landOnField(Player player) {
-			// TODO Auto-generated method stub
-			
-		}
+	@Override
+	public int getRent() {
+		return rent;
+	}
+		
 	
 }
 
