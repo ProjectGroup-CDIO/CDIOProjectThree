754f6501ef4cc5098f1061d02cfc73f86326c514
Martin Vieth <martin_123_vieth@hotmail.com>
2014-11-21 12:36:23 +0100

    Rettelser i die så den indeholder 2 terninge kast seperat opbevaret

--- src/gameEngine/Die.java
+++ src/gameEngine/Die.java
@@ -3,27 +3,35 @@ package gameEngine;
 public class Die {
 	
 	private final int MAX = 6; //max value of die
-	private int faceValue; //shows current value of die
-	private static int lastRoll;
+	private int faceValue1; //shows current value of roll 1
+	private int faceValue2; //Shows current value of roll 2
+	private int lastRoll;
 	
 	public int rollDie(){
 		
-		faceValue = (int) (Math.random() * MAX) + 1 + (int) (Math.random() * MAX) + 1;	
-		lastRoll = faceValue;		
-		return faceValue;
+		faceValue1 = (int) (Math.random() * MAX) + 1;
+		faceValue2 = (int) (Math.random() * MAX) + 1;	
+		lastRoll = faceValue1+faceValue2;		
+		return lastRoll;
 		
 	}
 
-	public int getFaceValue() {
-		return faceValue;
+	public int getFaceValue1() {
+		return faceValue1;
+	}
+	public int getFaceValue2(){
+		return faceValue2;
+	
 	}
 
-	public void setFaceValue(int faceValue) {
-		this.faceValue = faceValue;
-		lastRoll = faceValue;
+	public void setFaceValue1(int faceValue) {
+		this.faceValue1 = faceValue;
+	}
+	public void setFaceValue2(int faceValue) {
+		this.faceValue2 = faceValue;
 	}
 	
-	public static int getLastRoll(){
+	public int getLastRoll(){
 		return lastRoll;
 	}
 }
