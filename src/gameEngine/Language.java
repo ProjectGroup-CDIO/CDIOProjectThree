713e580ada238fecf2b3ab1b2c2cd9b953e0af1f
Delvaux Paulin Niyonzima <s144849@student.dtu.dk>
2014-11-26 16:17:06 +0100

    Slettet gammel kode fra language

--- src/gameEngine/Language.java
+++ src/gameEngine/Language.java
@@ -7,30 +7,11 @@ public class Language {
 
 	
 	public void languageSelection(String input) {
-		if(input.equals("Dansk")){
-			GUI.create("fields-Dansk.txt");
-			Game.setTypeNameOne("Indtast navn for spiller 1"); 
-			Game.setTypeNameTwo("Indtast navn for spiller 2");
-			Game.setRollDice("Kast Terning");
-			Game.setWon(" VANDT!!!");
-			Game.setIsWinner(" ER VINDEREN!!!");
-			Game.setDraw("SPILLET BLEV UAFGJORT");
-		}else if(input.equals("English")){
+		if(input.equals("English")){
 			GUI.create("fieldstext-cdio3.txt");
 			Game.setTypeNameOne("Enter name for player 1");
 			Game.setTypeNameTwo("Enter name for player 2");
 			Game.setRollDice("Roll Dice");
-			Game.setWon(" WON!!!");
-			Game.setIsWinner(" IS THE WINNER!!!");
-			Game.setDraw("THE GAME WAS A DRAW");
-		}else if(input.equals("Deutsch")){
-			GUI.create("fields-Deutsch.txt");
-			Game.setTypeNameOne("Geben Sie den Namen für den Spieler 1");
-			Game.setTypeNameTwo("Geben Sie den Namen für den Spieler 2");
-			Game.setRollDice("Würfeln");
-			Game.setWon(" GEWONNEN!!!");
-			Game.setIsWinner(" IST DER GEWINNER!!!");
-			Game.setDraw("DAS SPIEL WAR EIN UNENTSCHIEDEN"); 
 		}
 	}
 }
